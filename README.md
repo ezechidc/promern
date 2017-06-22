4f801e5b7b5b2b44fd48dbd86a0da013d4441fcd
dennis <ezechidc@yahoo.com>
2017-06-22 23:29:33 +0100

    chapter4 completed

---
+++ README.md
@@ -0,0 +1,1 @@
+This a MERN stack JIRA issues application clone based on the book Pro MERN stack by Subramanian.
--- static/src/App.jsx
+++ static/src/App.jsx
@@ -10,26 +10,21 @@ class IssueFilter extends React.Component {
     }
 }
 
-class IssueRow extends React.Component {
-    render() {
-        const issue = this.props.issue
-        return (
+const IssueRow = (props) => (
+        
             <tr>
-                <td>{issue.id}</td>
-                <td>{issue.status}</td>
-                <td>{issue.owner}</td>
-                <td>{issue.created.toDateString()}</td>
-                <td>{issue.effort}</td>
-                <td>{issue.completionDate ? issue.completionDate.toDateString(): ''}</td>
-                <td>{issue.title}</td>
+                <td>{props.issue.id}</td>
+                <td>{props.issue.status}</td>
+                <td>{props.issue.owner}</td>
+                <td>{props.issue.created.toDateString()}</td>
+                <td>{props.issue.effort}</td>
+                <td>{props.issue.completionDate ? props.issue.completionDate.toDateString(): ''}</td>
+                <td>{props.issue.title}</td>
             </tr>
-        );
-    }
-}
-
-class IssueTable extends React.Component {
-    render() {
-        const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
+    );
+    
+function IssueTable (props) {
+        const issueRows = props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
         return (
             <table className="bordered-table">
                 <thead>
@@ -47,12 +42,35 @@ class IssueTable extends React.Component {
             </table>
         );
     }
-}
+
 
 class IssueAdd extends React.Component {
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        var form = document.forms.issueAdd;
+        this.props.createIssue({
+            owner: form.owner.value,
+            title: form.title.value,
+            status: 'New',
+            created: new Date(),
+        });
+        // clear the form for next input
+        form.owner.value = ""; form.title.value = "";
+    }
+
     render() {
         return (
-            <div>This is a place for an add issue entry form.</div>
+            <div>
+                <form name="issueAdd" onSubmit={this.handleSubmit}>
+                    <input type="text" name="owner" placeholder="Owner" />
+                    <input type="text"  name="title" placeholder="Title" />
+                    <button>Add</button>
+                </form>
+            </div>
         );
     }
 }
@@ -72,15 +90,37 @@ const issues = [
 
 
 class IssueList extends React.Component {
+    constructor() {
+        super();
+        this.state = { issues: [] };
+        this.createIssue = this.createIssue.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    loadData() {
+        setTimeout(() => { this.setState({issues: issues})}, 500);
+    }
+
+    createIssue(newIssue) {
+        const newIssues = this.state.issues.slice();
+        newIssue.id = this.state.issues.length + 1;
+        newIssues.push(newIssue);
+        this.setState({ issues: newIssues });
+    }
+
+    
     render() {
         return (
             <div>
                 <hi>Issue Tracker</hi>
                 <IssueFilter />
                 <hr/>
-                <IssueTable issues={issues}/>
+                <IssueTable issues={this.state.issues}/>
                 <hr/>
-                <IssueAdd />
+                <IssueAdd createIssue={this.createIssue} />
             </div>
         );
     }
